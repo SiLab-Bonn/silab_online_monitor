bd86f07206df100611c41d4ed4b9f1faa1478b47
davidlp <pohl@physik.uni-bonn.de>
2015-10-17 13:31:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# SiLab online monitor
+This python package contains specific implementations of converters and receivers for several SiLab data acquisition systems. These implementations are used by the generic online_monitor python package to generate fast real time data analysis and plotting. 
+
+# Installation
+
+TBD
+
+# Usage
+
+TBD
+
---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages  # This setup relies on setuptools since distutils is insufficient and badly hacked code
+
+version = '0.0.1'
+author = 'David-Leon Pohl'
+author_email = 'pohl@physik.uni-bonn.de'
+
+# requirements for core functionality from requirements.txt
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+setup(
+    name='silab_online_monitor',
+    version=version,
+    description='Specific converters and receivers for SiLab data acquisition systems to be used with the generic online_monitor python package.',
+    url='https://github.com/SiLab-Bonn/silab_online_monitor',
+    license='MIT License',
+    long_description='',
+    author=author,
+    maintainer=author,
+    author_email=author_email,
+    maintainer_email=author_email,
+    install_requires=install_requires,
+    packages=find_packages(),
+    include_package_data=True,  # accept all data files and directories matched by MANIFEST.in or found in source control
+    package_data={'': ['README.*', 'VERSION'], 'docs': ['*'], 'examples': ['*']},
+    keywords=['silab', 'online monitor', 'real time plots'],
+    platforms='any'
+)
+
+import os
+from online_monitor.utils import settings
+settings.add_converter_path(os.path.dirname(os.path.realpath(__file__)) + r'\converter')
+settings.add_receiver_path(os.path.dirname(os.path.realpath(__file__)) + r'\receiver')
