2d2c8eae0b4cc9e43d0b18f53c7cc5621ca4bf88
DavidLP <david-leon.pohl@rub.de>
2016-04-09 20:22:04 +0200

    ENH: histogrammer converter to create histograms from the FE-I4 hits and status values

---
+++ silab_online_monitor/converter/pybar_fei4_histogrammer.py
@@ -0,0 +1,145 @@
+''' Histograms the ATLAS-FEI4 hit table and integrated the interpretation histograms'''
+
+import time
+from zmq.utils import jsonapi
+import numpy as np
+
+# Online monitor imports
+from online_monitor.converter.transceiver import Transceiver
+from online_monitor.utils import utils
+
+# pyBAR related imports
+from pybar_fei4_interpreter.data_histograming import PyDataHistograming
+
+
+class PybarFEI4Histogrammer(Transceiver):
+
+    def setup_transceiver(self):
+        self.set_bidirectional_communication()  # We want to be able to change the histogrammmer settings
+
+    def setup_interpretation(self):
+        self.histograming = PyDataHistograming()
+        self.histograming.set_no_scan_parameter()
+        self.histograming.create_occupancy_hist(True)
+        self.histograming.create_rel_bcid_hist(True)
+        self.histograming.create_tot_hist(True)
+        self.histograming.create_tdc_hist(True)
+        # Variables
+        self.n_readouts = 0
+        self.readout = 0
+        self.fps = 0  # data frames per second
+        self.hps = 0  # hits per second
+        self.eps = 0  # events per second
+        self.plot_delay = 0
+        self.total_hits = 0
+        self.total_events = 0
+        self.updateTime = time.time()
+        # Histogrammes from interpretation stored for summing
+        self.tdc_counters = None
+        self.error_counters = None
+        self.service_records_counters = None
+        self.trigger_error_counters = None
+
+    def deserialze_data(self, data):
+        return jsonapi.loads(data, object_hook=utils.json_numpy_obj_hook)
+
+    def interpret_data(self, data):
+        def update_monitor(self, timestamp_start, timestamp_stop, readout_error, scan_parameters, n_hits, n_events):
+#             self.plot_delay = self.plot_delay * 0.9 + (now - timestamp_stop) * 0.1
+#             self.plot_delay_label.setText("Plot Delay\n%s" % ((time.strftime('%H:%M:%S', time.gmtime(self.plot_delay))) if abs(self.plot_delay) > 5 else "%1.2f ms" % (self.plot_delay * 1.e3)))
+            recent_fps = 1.0 / (now - self.updateTime)  # calculate FPS
+            recent_hps = (recent_total_hits - self.total_hits) / (now - self.updateTime)
+            recent_eps = (recent_total_events - self.total_events) / (now - self.updateTime)
+            self.updateTime = now
+            self.total_hits = recent_total_hits
+            self.total_events = recent_total_events
+            self.fps = self.fps * 0.7 + recent_fps * 0.3
+            self.hps = self.hps + (recent_hps - self.hps) * 0.3 / self.fps
+            self.eps = self.eps + (recent_eps - self.eps) * 0.3 / self.fps
+            self.update_rate(self.fps, self.hps, recent_total_hits, self.eps, recent_total_events)
+ 
+        if 'meta_data' in data[0][1]:  # Meta data is directly forwarded to the receiver, only hit data, event counters are histogramed; 0 from frontend index, 1 for data dict
+            meta_data = data[0][1]['meta_data']
+            now = time.time()
+            recent_total_hits = meta_data['n_hits']
+            recent_total_events = meta_data['n_events']
+            recent_fps = 1.0 / (now - self.updateTime)  # calculate FPS
+            recent_hps = (recent_total_hits - self.total_hits) / (now - self.updateTime)
+            recent_eps = (recent_total_events - self.total_events) / (now - self.updateTime)
+            self.updateTime = now
+            self.total_hits = recent_total_hits
+            self.total_events = recent_total_events
+            self.fps = self.fps * 0.7 + recent_fps * 0.3
+            self.hps = self.hps + (recent_hps - self.hps) * 0.3 / self.fps
+            self.eps = self.eps + (recent_eps - self.eps) * 0.3 / self.fps
+            meta_data.update({'fps': self.fps, 'hps': self.hps, 'total_hits': self.total_hits, 'eps': self.eps, 'total_events': self.total_events})
+            return [data[0][1]]
+
+        self.readout += 1
+
+        if self.n_readouts != 0:  # = 0 for infinite integration
+            if self.readout % self.n_readouts == 0:
+                self.histograming.reset()
+                self.tdc_counters = np.zeros_like(self.tdc_counters)
+                self.error_counters = np.zeros_like(self.error_counters)
+                self.service_records_counters = np.zeros_like(self.service_records_counters)
+                self.trigger_error_counters = np.zeros_like(self.trigger_error_counters)
+                self.readouts = 0
+
+        interpreted_data = data[0][1]
+
+        self.histograming.add_hits(interpreted_data['hits'])
+
+        # Sum up interpreter histograms
+        if self.tdc_counters is not None:
+            self.tdc_counters += interpreted_data['tdc_counters']
+        else:
+            self.tdc_counters = interpreted_data['tdc_counters'].copy()  # Copy needed to give ownage to histogrammer
+        if self.error_counters is not None:
+            self.error_counters += interpreted_data['error_counters']
+        else:
+            self.error_counters = interpreted_data['error_counters'].copy()  # Copy needed to give ownage to histogrammer
+        if self.service_records_counters is not None:
+            self.service_records_counters += interpreted_data['service_records_counters']
+        else:
+            self.service_records_counters = interpreted_data['service_records_counters'].copy()  # Copy needed to give ownage to histogrammer
+        if self.trigger_error_counters is not None:
+            self.trigger_error_counters += interpreted_data['trigger_error_counters']
+        else:
+            self.trigger_error_counters = interpreted_data['trigger_error_counters'].copy()  # Copy needed to give ownage to histogrammer
+
+        histogrammed_data = {
+            'occupancy': self.histograming.get_occupancy(),
+            'tot_hist': self.histograming.get_tot_hist(),
+            'tdc_counters': self.tdc_counters,
+            'error_counters': self.error_counters,
+            'service_records_counters': self.service_records_counters,
+            'trigger_error_counters': self.trigger_error_counters,
+            'rel_bcid_hist': self.histograming.get_rel_bcid_hist()
+        }
+
+        return [histogrammed_data]
+
+    def serialze_data(self, data):
+        return jsonapi.dumps(data, cls=utils.NumpyEncoder)
+
+    def handle_command(self, command):
+        if command[0] == 'RESET':
+            self.histograming.reset()
+            self.tdc_counters = np.zeros_like(self.tdc_counters)
+            self.error_counters = np.zeros_like(self.error_counters)
+            self.service_records_counters = np.zeros_like(self.service_records_counters)
+            self.trigger_error_counters = np.zeros_like(self.trigger_error_counters)
+        else:
+            self.n_readouts = int(command[0])
+
+    def update_rate(self, fps, hps, recent_total_hits, eps, recent_total_events):
+        self.rate_label.setText("Readout Rate\n%d Hz" % fps)
+        if self.spin_box.value() == 0:  # show number of hits, all hits are integrated
+            self.hit_rate_label.setText("Total Hits\n%d" % int(recent_total_hits))
+        else:
+            self.hit_rate_label.setText("Hit Rate\n%d Hz" % int(hps))
+        if self.spin_box.value() == 0:  # show number of events
+            self.event_rate_label.setText("Total Events\n%d" % int(recent_total_events))
+        else:
+            self.event_rate_label.setText("Event Rate\n%d Hz" % int(eps))
