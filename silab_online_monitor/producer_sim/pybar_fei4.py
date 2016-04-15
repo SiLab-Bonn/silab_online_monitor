e33bbf8cfb7a6aa1a32ff99185f97582cfa60a78
DavidLP <david-leon.pohl@rub.de>
2016-04-15 15:39:30 +0200

    ENH: support scans without scan parameter

--- silab_online_monitor/producer_sim/pybar_fei4.py
+++ silab_online_monitor/producer_sim/pybar_fei4.py
@@ -16,27 +16,39 @@ class pyBarFEI4Sim(ProducerSim):
         with tb.openFile(self.config['data_file'], mode="r") as in_file_h5:
             self.meta_data = in_file_h5.root.meta_data[:]
             self.raw_data = in_file_h5.root.raw_data[:]
-            self.scan_parameter_name = in_file_h5.root.scan_parameters.dtype.names
-            self.scan_parameters = in_file_h5.root.scan_parameters[:]
+            self.n_readouts = self.meta_data.shape[0]
+
+            try:
+                self.scan_parameter_name = in_file_h5.root.scan_parameters.dtype.names
+                self.scan_parameters = in_file_h5.root.scan_parameters[:]
+            except tb.NoSuchNodeError:
+                self.scan_parameter_name = 'No parameter'
+                self.scan_parameters = None
+
             self.readout_word_indeces = np.column_stack((self.meta_data['index_start'], self.meta_data['index_stop']))
             self.actual_readout = 0
             self.last_readout_time = None
 
     def get_data(self):  # Return the data of one readout
-        if self.actual_readout < self.scan_parameters.shape[0]:
+        if self.actual_readout < self.n_readouts:
             index_start, index_stop = self.readout_word_indeces[self.actual_readout]
             data = []
             data.append(self.raw_data[index_start:index_stop])
             data.extend((float(self.meta_data[self.actual_readout]['timestamp_start']), float(self.meta_data[self.actual_readout]['timestamp_stop']), int(self.meta_data[self.actual_readout]['error'])))
-            # FIXME: Simple syncronization to have replay with similar timing, does not really work
+            # FIXME: Simple syncronization to replay with similar timing, does not really work
             now = time.time()
+
             if self.last_readout_time is not None:
                 delay = now - self.last_readout_time
                 additional_delay = self.meta_data[self.actual_readout]['timestamp_stop'] - self.meta_data[self.actual_readout]['timestamp_start'] - delay
                 if additional_delay > 0:
                     time.sleep(additional_delay)
             self.last_readout_time = now
-            return data, {str(self.scan_parameter_name): int(self.scan_parameters[self.actual_readout][0])}
+
+            if self.scan_parameters is not None:
+                return data, {str(self.scan_parameter_name): int(self.scan_parameters[self.actual_readout][0])}
+            else:
+                return data, {'No parameter': 0}
 
     def send_data(self):
         '''Sends the data of every read out (raw data and meta data) via ZeroMQ to a specified socket
