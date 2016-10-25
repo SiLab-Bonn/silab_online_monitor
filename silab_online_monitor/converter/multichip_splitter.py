0da36756efc1956de1d7237555ca7531a56cb598
Nandita Khetan <khetan@physik.uni-bonn.de>
2016-10-25 17:57:07 +0200

    ENH: Try fixing tdc word selection

--- silab_online_monitor/converter/multichip_splitter.py
+++ silab_online_monitor/converter/multichip_splitter.py
@@ -34,18 +34,35 @@ class MultichipSplitter(Transceiver):
         return {'meta_data': self.meta_data}
 
     def interpret_data(self, data):
-        if isinstance(data[0][1], dict):  # Meta data is saved for next raw data send
+        if isinstance(data[0][1], dict):  # Meta data is saved in deserialze_data for next raw data send
             return
 
         raw_data = data[0][1]
         splitted_data = [None] * len(self.backends)
-        
+
         for backend_index in range(len(self.backends)):
-            selection = np.bitwise_and(raw_data, 0x0F000000) == np.left_shift(backend_index, 24)
+            
+            selection_frontend = np.bitwise_and(raw_data, 0xFF000000) == np.left_shift(backend_index + 1, 24) 
+            selection_tdc = np.bitwise_and(raw_data, 0xF0000000) == np.left_shift(3 + 1, 28)
+            selection_trigger = np.bitwise_and(raw_data, 0x80000000) == np.left_shift(1, 31)
+            
+#             select = np.greater(np.bitwise_and(raw_data, 0b01110000000000000000000000000000), 0)
+#             raw_data[select] = np.bitwise_and(raw_data[select], 0x0FFFFFFF)
+#             raw_data[select] = np.bitwise_or(raw_data[select], 0x10000000)
+            
+            
+#            print "selection_frontend", np.count_nonzero(selection_frontend)
+#            print "selection_frontend", raw_data[selection_frontend]
+#            print "selection_tdc", np.count_nonzero(selection_tdc)
+#            print "selection_tdc", raw_data[selection_tdc]
+            selection = np.logical_or(np.logical_or(selection_frontend, selection_tdc), selection_trigger)
+            
+#            print "selection", np.count_nonzero(selection)
+#            print "selection", raw_data[selection] 
             splitted_data[backend_index] = raw_data[selection]
-
+#            print "selection" , splitted_data
         return splitted_data
-
+        
     def serialze_data(self, data):
         return np.zeros(10)
         #return jsonapi.dumps(data, cls=utils.NumpyEncoder)
