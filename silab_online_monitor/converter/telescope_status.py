d3a9b6a68ea094cf87e1cc470073eaf4c360f346
leloup314 <s6plwolf@uni-bonn.de>
2016-12-20 01:04:33 +0100

    ENH: working telescope status receiver; MAINT: add missing file Colorbar.py for beast2.py receiver

--- silab_online_monitor/converter/telescope_status.py
+++ silab_online_monitor/converter/telescope_status.py
@@ -16,7 +16,15 @@ class TelescopeStatus(Transceiver):
         # variables to determine whether to do sth or not
         self.active_tab = None  # stores name (str) of active tab in online monitor
         self.tel_stat_tab = 'Telescope_Status'  # store name (str) of Telescope_Status tab
-
+        
+        # dict for status data
+        #self.status_data = np.zeros(shape=(10), dtype=[('m26_voltage','f8'),('m26_current','f8'),('vdda_v','f8'),('vdda_c','f8'),('vddd_v','f8'),('vddd_c','f8')])
+        self.status_data = 0
+        # start timer
+        self.start_time = time.time()
+        # time axis
+        self.time_axis = []
+        
     def deserialze_data(self, data):  # According to pyBAR data serilization
         datar, meta = utils.simple_dec(data)
         if 'hits' in meta:
@@ -27,7 +35,25 @@ class TelescopeStatus(Transceiver):
         
         if self.active_tab != self.tel_stat_tab:  # if active tab in online monitor is not telescope status, return
             return
-        return data
+        
+        for actual_data in data:
+
+            if 'meta_data' in actual_data[1]:  # meta_data is skipped
+                continue
+            if 'status' in actual_data[1]:
+                if actual_data[1]['status'].shape[0] = 0: # empty array is skipped
+                    continue
+                else:
+                    time = time.time() - self.start_time
+                    self.time_axis.append(time)
+                    self.status_data = actual_data[1]['status']
+                    
+        return [{'status': self.status_data, 'time': self.time_axis}]
+        #~ for name in self.status_data.dtype.names:
+            #~ print name
+            #~ self.status_data[name] = np.random.uniform(low=1.0,high=8.0, size=self.status_data.shape[0])
+            
+        #~ return [self.status_data]
 
     def serialze_data(self, data):
         return jsonapi.dumps(data, cls=utils.NumpyEncoder)
@@ -36,3 +62,21 @@ class TelescopeStatus(Transceiver):
         
         if 'ACTIVETAB' in command[0]:  # received signal is 'ACTIVETAB tab' where tab is the name (str) of the selected tab in online monitor
             self.active_tab = str(command[0].split()[1])
+            
+        if 'RESET' in command[0]:
+            if 'M26' in command[0]:
+                if 'CURRENT' in command[0]:
+                    print 'm26_current'
+                    self.status_data['m26_current'] = 0
+                elif 'VOLTAGE' in command[0]:
+                    print 'm26_voltage'
+                    self.status_data['m26_voltage'] = 0
+            elif 'FEI4' in command[0]:
+                if 'VDDA' in command[0]:
+                    print 'VDDA'
+                    self.status_data['vdda_v'] = 0
+                    self.status_data['vdda_c'] = 0
+                elif 'VDDD' in command[0]:
+                    print 'VDDD'
+                    self.status_data['vddd_v'] = 0
+                    self.status_data['vddd_c'] = 0
