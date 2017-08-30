b2395c552d630a18bc6c9f0d6fe2fdf836745e88
leloup314 <s6plwolf@uni-bonn.de>
2017-08-31 01:04:29 +0200

    ENH: hot pixel removal for anemone_correlator; MAINT: rename

--- silab_online_monitor/receiver/anemone_correlator.py
+++ silab_online_monitor/receiver/anemone_correlator.py
@@ -1,6 +1,7 @@
 from online_monitor.receiver.receiver import Receiver
 from zmq.utils import jsonapi
 import numpy as np
+from numba import jit
 
 from PyQt5 import Qt
 import pyqtgraph as pg
@@ -8,6 +9,23 @@ from pyqtgraph.Qt import QtGui
 from pyqtgraph.dockarea import DockArea, Dock
 
 from online_monitor.utils import utils
+
+
+@jit
+def noisy_calc(nparray):
+
+    # use np.ravel to flatten since it returns view and is much faster than flatten()
+    a = nparray.ravel()
+
+    # remove all zeroes from flattened view
+    b = a[a != 0]
+
+    # only take entries above the mean
+    c = b[b >= np.mean(b)]
+
+    # the ratio of the mean of all values between the mean and maximum and the maximum occupancy for
+    # normal (not noisy) hit pixels scales with an almost constant factor (~ 7 empirically)
+    return np.mean(c), np.amax(c)
 
 
 class HitCorrelator(Receiver):
@@ -22,8 +40,10 @@ class HitCorrelator(Receiver):
 
     def setup_widgets(self, parent, name):
 
-        self.occupancy_images_columns = {}
-        self.occupancy_images_rows = {}
+        self.rm_hot_pixels = False
+
+        self.occupancy_image_columns = None
+        self.occupancy_image_rows = None
 
         DUTS = []
 
@@ -90,16 +110,16 @@ class HitCorrelator(Receiver):
         reset_button.setMaximumSize(300, 30)
         layout.setHorizontalSpacing(25)
         layout.addWidget(reset_button, 0, 1, 0, 1)
-        remove_background_checkbox = QtGui.QCheckBox('Remove background:')
-        layout.addWidget(remove_background_checkbox, 0, 2, 1, 1)
-        remove_background_spinbox = QtGui.QDoubleSpinBox()
-        remove_background_spinbox.setRange(0.0, 100.0)
-        remove_background_spinbox.setValue(99.0)
-        remove_background_spinbox.setSingleStep(1.0)
-        remove_background_spinbox.setDecimals(1)
-        remove_background_spinbox.setPrefix('< ')
-        remove_background_spinbox.setSuffix(' % maximum occupancy')
-        layout.addWidget(remove_background_spinbox, 0, 3, 1, 1)
+        rm_hot_pixels_checkbox = QtGui.QCheckBox('Remove hot pixels:')
+        layout.addWidget(rm_hot_pixels_checkbox, 0, 2, 1, 1)
+        rm_hot_pixels_spinbox = QtGui.QDoubleSpinBox()
+        rm_hot_pixels_spinbox.setRange(0.0, 100.0)
+        rm_hot_pixels_spinbox.setValue(7.0)
+        rm_hot_pixels_spinbox.setSingleStep(1.0)
+        rm_hot_pixels_spinbox.setDecimals(1)
+        rm_hot_pixels_spinbox.setPrefix('> ')
+        rm_hot_pixels_spinbox.setSuffix(' x avg. center occupancy')
+        layout.addWidget(rm_hot_pixels_spinbox, 0, 3, 1, 1)
         self.transpose_checkbox = QtGui.QCheckBox('Transpose columns and rows (FE-I4)')
         layout.addWidget(self.transpose_checkbox, 1, 3, 1, 1)
         self.convert_checkbox = QtGui.QCheckBox('Axes in ' + u'\u03BC' + 'm')
@@ -109,8 +129,6 @@ class HitCorrelator(Receiver):
         dock_status.addWidget(cw)
         reset_button.clicked.connect(lambda: self.send_command('RESET'))
         self.transpose_checkbox.stateChanged.connect(lambda value: self.send_command('TRANSPOSE %d' % value))
-        remove_background_checkbox.stateChanged.connect(lambda value: self.send_command('BACKGROUND %d' % value))
-        remove_background_spinbox.valueChanged.connect(lambda value: self.send_command('PERCENTAGE %f' % value))
 
         # Add plot docks for column corr
         occupancy_graphics1 = pg.GraphicsLayoutWidget()
@@ -130,7 +148,7 @@ class HitCorrelator(Receiver):
         self.plot1.getAxis('left').setLabel(text='Columns')
         self.plot1.addItem(occupancy_img_col)
         dock_corr_column.addWidget(self.plot1)
-        self.occupancy_images_columns = occupancy_img_col
+        self.occupancy_image_columns = occupancy_img_col
         # Add plot docks for row corr
         occupancy_graphics2 = pg.GraphicsLayoutWidget()
         occupancy_graphics2.show()
@@ -144,12 +162,25 @@ class HitCorrelator(Receiver):
         self.plot2.getAxis('left').setLabel(text='Rows')
         self.plot2.addItem(occupancy_img_rows)
         dock_corr_row.addWidget(self.plot2)
-        self.occupancy_images_rows = occupancy_img_rows
+        self.occupancy_image_rows = occupancy_img_rows
         #
         dock_area.addDock(dock_status, 'top')
         dock_area.addDock(dock_select_duts, 'left')
         dock_area.addDock(dock_corr_column, 'bottom')
         dock_area.addDock(dock_corr_row, 'right', dock_corr_column)
+
+        # function to set rm hot pixel flag and set factor
+        def config_hot_pixels():
+
+            if rm_hot_pixels_checkbox.isChecked():
+                self.factor = rm_hot_pixels_spinbox.value()
+                self.rm_hot_pixels = True
+
+            else:
+                self.rm_hot_pixels = False
+
+        rm_hot_pixels_checkbox.stateChanged.connect(config_hot_pixels)
+        rm_hot_pixels_spinbox.valueChanged.connect(config_hot_pixels)
 
         # function to label and scale axis in um
         def scale_and_label_axes(scale_state, dut1, dut2, dut1_name, dut2_name, transpose_state):
@@ -330,10 +361,53 @@ class HitCorrelator(Receiver):
 
                 if 'column' == key:
 
-                    self.occupancy_images_columns.setImage(data[key][:, :], autoDownsample=True)
+                    self.occupancy_image_columns.setImage(data[key][:, :], autoDownsample=True)
+
+                    if self.rm_hot_pixels:
+
+                        occ_upper_mean, occ_max = noisy_calc(self.occupancy_image_columns.image)
+
+                        if occ_max > (self.factor * occ_upper_mean):
+
+                            # make another copy of occupancy to change its entries;
+                            occupancy = self.occupancy_image_columns.image.copy()
+
+                            # remove every pixel with a occupancy higher than this factor times the mean
+                            occupancy[occupancy > (self.factor * occ_upper_mean)] = 0
+
+                            # clear occupancy image
+                            self.occupancy_image_columns.clear()
+
+                            # set new occupancy; free from hot pixels
+                            self.occupancy_image_columns.setImage(occupancy, autoDownsample=True)
+
+                        else:
+                            pass
 
                 if 'row' == key:
 
-                    self.occupancy_images_rows.setImage(data[key][:, :], autoDownsample=True)
+                    self.occupancy_image_rows.setImage(data[key][:, :], autoDownsample=True)
+
+                    if self.rm_hot_pixels:
+
+                        occ_upper_mean, occ_max = noisy_calc(self.occupancy_image_rows.image)
+
+                        if occ_max > (self.factor * occ_upper_mean):
+
+                            # make another copy of occupancy to change its entries;
+                            occupancy = self.occupancy_image_rows.image.copy()
+
+                            # remove every pixel with a occupancy higher than this factor times the mean
+                            occupancy[occupancy > (self.factor * occ_upper_mean)] = 0
+
+                            # clear occupancy image
+                            self.occupancy_image_rows.clear()
+
+                            # set new occupancy; free from hot pixels
+                            self.occupancy_image_rows.setImage(occupancy, autoDownsample=True)
+
+                        else:
+                            pass
+
         else:
             self.rate_label.setText('Readout Rate: %d Hz' % data['meta_data']['fps'])
