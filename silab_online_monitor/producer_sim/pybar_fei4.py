6dc8be7b4bb9e4472a6f2abc324931cfcb1422ce
DavidLP <pohl@physik.uni-bonn.de>
2018-07-16 13:27:20 +0200

    ENH: fix send delay MAINT: cleanup

--- silab_online_monitor/producer_sim/pybar_fei4.py
+++ silab_online_monitor/producer_sim/pybar_fei4.py
@@ -6,6 +6,8 @@ import tables as tb
 import zmq
 import logging
 
+from pybar.daq.fei4_raw_data import send_data
+
 from online_monitor.utils.producer_sim import ProducerSim
 
 
@@ -13,76 +15,67 @@ class pyBarFEI4Sim(ProducerSim):
 
     def setup_producer_device(self):
         ProducerSim.setup_producer_device(self)
-        self.in_file_h5 = tb.open_file(self.config['data_file'], mode="r")
-        self.meta_data = self.in_file_h5.root.meta_data[:]
-        self.raw_data = self.in_file_h5.root.raw_data
-        self.n_readouts = self.meta_data.shape[0]
-        self.total_data = 0  # amount of replayed data in MB
-        self.time_start = time.time()  # calculate duration of replay
-        self.time_end = 0  # calculate duration of replay
+        self.delay = float(self.config.get('delay', 0.))
 
-        try:
-            self.scan_parameter_name = self.in_file_h5.root.scan_parameters.dtype.names
-            self.scan_parameters = self.in_file_h5.root.scan_parameters[:]
-        except tb.NoSuchNodeError:
-            self.scan_parameter_name = 'No parameter'
-            self.scan_parameters = None
+    def _get_data(self):  # Return the data of one readout
+        ''' Yield data of one readout
 
-        self.readout_word_indeces = np.column_stack((self.meta_data['index_start'], self.meta_data['index_stop']))
-        self.actual_readout = 0
-        self.last_readout_time = None
+            Delay return if replay is too fast
+        '''
+        with tb.open_file(self.config['data_file'], mode="r") as in_file_h5:
+            meta_data = in_file_h5.root.meta_data[:]
+            raw_data = in_file_h5.root.raw_data
+            n_readouts = meta_data.shape[0]
 
-    def get_data(self):  # Return the data of one readout
-        if self.actual_readout < self.n_readouts:
-            index_start, index_stop = self.readout_word_indeces[self.actual_readout]
-            data = []
-            data.append(self.raw_data[index_start:index_stop])
-            data.extend((float(self.meta_data[self.actual_readout]['timestamp_start']), float(self.meta_data[self.actual_readout]['timestamp_stop']), int(self.meta_data[self.actual_readout]['error'])))
-            # FIXME: Simple syncronization to replay with similar timing, does not really work
-            now = time.time()
+            try:
+                self.scan_parameter_name = in_file_h5.root.scan_parameters.dtype.names
+                self.scan_parameters = in_file_h5.root.scan_parameters[:]
+            except tb.NoSuchNodeError:
+                self.scan_parameter_name = 'No parameter'
+                self.scan_parameters = None
 
-            if self.last_readout_time is not None:
+            self.last_readout_time = time.time()
+
+            for i in range(n_readouts):
+                # Raw data indeces of readout
+                i_start = meta_data['index_start'][i]
+                i_stop = meta_data['index_stop'][i]
+
+                # Time stamp of readout
+                t_start = meta_data[i]['timestamp_start']
+
+                data = []
+                data.append(raw_data[i_start:i_stop])
+                data.extend((float(meta_data[i]['timestamp_start']),
+                             float(meta_data[i]['timestamp_stop']),
+                             int(meta_data[i]['error'])))
+
+                # Determine replay delays
+                if i == 0:  # Initialize on first readout
+                    self.last_timestamp_start = t_start
+                now = time.time()
                 delay = now - self.last_readout_time
-                additional_delay = self.meta_data[self.actual_readout]['timestamp_stop'] - self.meta_data[self.actual_readout]['timestamp_start'] - delay
+                additional_delay = t_start - self.last_timestamp_start - delay
                 if additional_delay > 0:
+                    # Wait if send too fast, especially needed when readout was
+                    # stopped during data taking (e.g. for mask shifting)
                     time.sleep(additional_delay)
-            self.last_readout_time = now
+                self.last_readout_time = time.time()
+                self.last_timestamp_start = t_start
 
-            if self.scan_parameters is not None:
-                return data, {str(self.scan_parameter_name): int(self.scan_parameters[self.actual_readout][0])}
-            else:
-                return data, {'No parameter': 0}
+                if self.scan_parameters is not None:
+                    yield data, {str(self.scan_parameter_name): int(self.scan_parameters[i][0])}
+                else:
+                    yield data, {'No parameter': 0}
 
     def send_data(self):
-        '''Sends the data of every read out (raw data and meta data) via ZeroMQ to a specified socket
+        '''Sends the data of every read out (raw data and meta data)
+
+            Sending via ZeroMQ to a specified socket.
         '''
-        time.sleep(float(self.config['delay']))  # Delay is given in seconds
-
-        try:
-            data, scan_parameters = self.get_data()  # Get data of actual readout
-        except TypeError:  # Data is fully replayes
-            self.time_end = time.time()
-            logging.warning('%s producer: No data to replay anymore! Data send in %.2f s is %.2f MB' % (self.name, self.time_end - self.time_start, self.total_data / (1024.0 ** 2)))  # show amount of sent data after replay ended
-            time.sleep(10)
-            return
-
-        self.actual_readout += 1
-
-        data_meta_data = dict(
-            name='ReadoutData',
-            dtype=str(data[0].dtype),
-            shape=data[0].shape,
-            timestamp_start=data[1],  # float
-            timestamp_stop=data[2],  # float
-            readout_error=data[3],  # int
-            scan_parameters=scan_parameters  # dict
-        )
-        try:
-            self.total_data += data[0].nbytes  # sum up sent data packages
-            self.sender.send_json(data_meta_data, flags=zmq.SNDMORE | zmq.NOBLOCK)
-            self.sender.send(data[0], flags=zmq.NOBLOCK)  # PyZMQ supports sending numpy arrays without copying any data
-        except zmq.Again:
-            pass
+        for data, scan_parameters in self._get_data():
+            time.sleep(self.delay)
+            send_data(socket=self.sender, data=data, scan_parameters=scan_parameters)
 
     def __del__(self):
         self.in_file_h5.close()
